b4b34259c968fcc076583a4cc0ced980f4d9fdfe
jaesung lim <lim4057@naver.com>
2019-10-18 00:06:18 +0900

    user-annotation add

--- src/main/java/com/spring/sample/user/impl/UserServiceImpl.java
+++ src/main/java/com/spring/sample/user/impl/UserServiceImpl.java
@@ -1,9 +1,14 @@
 package com.spring.sample.user.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.spring.sample.user.UserService;
 import com.spring.sample.user.UserVO;
 
+@Service("userService")
 public class UserServiceImpl implements UserService{
+	@Autowired
 	private UserDAO userDAO;
 	
 	public void setUserDAO(UserDAO userDAO) {
