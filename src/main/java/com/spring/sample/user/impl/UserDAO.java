af55b0875a15763f39e309c3161bcbfec1d1a254
Your Name <lim4057@naver.com>
2019-10-20 16:44:59 +0900

    JDBCUtil.java delete

--- src/main/java/com/spring/sample/user/impl/UserDAO.java
+++ src/main/java/com/spring/sample/user/impl/UserDAO.java
@@ -4,13 +4,17 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
-import com.spring.sample.common.JDBCUtil;
 import com.spring.sample.user.UserVO;
 
 @Repository("UserDAO")
 public class UserDAO {
+	
+	@Autowired
+	JdbcTemplate jdbctemplate;
 	
 	private Connection conn = null;
 	private PreparedStatement stmt = null;
@@ -20,27 +24,32 @@ public class UserDAO {
 	
 	
 	public UserVO getUser(UserVO vo) {
-		UserVO user = null;
-		try {
-			System.out.println("===> JDBC로 getUser() 기능 처리");
-			conn = JDBCUtil.getConnection();
-			stmt = conn.prepareStatement(USER_GET);
-			stmt.setString(1, vo.getId());
-			stmt.setString(2, vo.getPassword());
-			rs = stmt.executeQuery();
-			if(rs.next())
-			{
-				user = new UserVO();
-				user.setId(rs.getString("ID"));
-				user.setPassword(rs.getString("PASSWORD"));
-				user.setName(rs.getString("NAME"));
-				user.setRole(rs.getString("ROLE"));
-			}
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		System.out.println("===> JDBC로 getUser() 기능 처리");
+		Object[] args = {vo.getId() ,vo.getPassword()};
+		return jdbctemplate.queryForObject(USER_GET, args, new UserRowMapper());
 		
-		return user;
+		
+//		UserVO user = null;
+//		try {
+//			System.out.println("===> JDBC로 getUser() 기능 처리");
+//			conn = JDBCUtil.getConnection();
+//			stmt = conn.prepareStatement(USER_GET);
+//			stmt.setString(1, vo.getId());
+//			stmt.setString(2, vo.getPassword());
+//			rs = stmt.executeQuery();
+//			if(rs.next())
+//			{
+//				user = new UserVO();
+//				user.setId(rs.getString("ID"));
+//				user.setPassword(rs.getString("PASSWORD"));
+//				user.setName(rs.getString("NAME"));
+//				user.setRole(rs.getString("ROLE"));
+//			}
+//			
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		
+//		return user;
 	}
 }
