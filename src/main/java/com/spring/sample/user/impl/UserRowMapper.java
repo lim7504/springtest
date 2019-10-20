fad241b82b9587d327370933dc48a9154b1aaed5
Your Name <lim4057@naver.com>
2019-10-20 16:44:59 +0900

    JDBCUtil.java delete

---
+++ src/main/java/com/spring/sample/user/impl/UserRowMapper.java
@@ -0,0 +1,20 @@
+package com.spring.sample.user.impl;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.spring.sample.user.UserVO;
+
+public class UserRowMapper implements RowMapper<UserVO>{
+	public UserVO mapRow(ResultSet rs, int rowNum) throws SQLException{
+		UserVO user = new UserVO();
+		user.setId(rs.getString("ID"));
+		user.setName(rs.getString("NAME"));
+		user.setPassword(rs.getString("PASSWORD"));
+		user.setRole(rs.getString("ROLE"));
+		return user;
+	}
+	
+}
