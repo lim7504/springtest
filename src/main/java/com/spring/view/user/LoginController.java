48e659ebccd5a8234ee3d7315479af5e7f2e0cf2
jslim <lim4057@naver.com>
2019-10-22 18:13:45 +0900

    spring MVC annotation add

--- src/main/java/com/spring/view/user/LoginController.java
+++ src/main/java/com/spring/view/user/LoginController.java
@@ -1,37 +1,30 @@
 package com.spring.view.user;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.sample.user.UserVO;
 import com.spring.sample.user.impl.UserDAO;
 
-
-public class LoginController implements Controller{
+@Controller
+public class LoginController {
 	
-	@Override
-	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) {
-		System.out.println("로그인 처리");
+	@RequestMapping(value="/login.do",method = RequestMethod.GET)
+	public String loginView(UserVO vo) {
 		
-		String id = request.getParameter("id");
-		String password = request.getParameter("password");
-		
-		UserVO vo = new UserVO();
-		vo.setId(id);
-		vo.setPassword(password);
-		
-		UserDAO userDAO = new UserDAO();
-		UserVO user = userDAO.getUser(vo);
-		
-		ModelAndView mav = new ModelAndView();
-		if(user != null) {
-			mav.setViewName("redirect:getBoardList.do");
-		} else {
-			mav.setViewName("redirect:login.jsp");
-		}
-		return mav;
+		vo.setId("test");
+		vo.setPassword("test123");
+		return "login.jsp";
 	}
+	
+	@RequestMapping(value="/login.do",method = RequestMethod.POST)
+	public String login(UserVO vo, UserDAO userDAO) {
+		System.out.println("로그인 처리");
+		if(userDAO.getUser(vo) != null) return "getBoardList.do";
+		else return "login.jsp";
+	}
+
 }
